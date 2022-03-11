cf1250ab206266eb2e611e021a70a3eab2c906d2
Nolan Bradshaw <nolanbradshaw22@gmail.com>
2022-03-11 13:31:25 -0500

    Parsing yaml to a flex container

---
+++ src/Flex/Parsers/YamlParser.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using YamlDotNet.RepresentationModel;
+
+namespace Flex.Parsers
+{
+    internal static class YamlParser
+    {
+        internal static Dictionary<string, string> ParseToDictionary(YamlMappingNode mappingNode)
+        {
+            var dataDict = new Dictionary<string, string>();
+            foreach (var child in mappingNode.Children)
+            {
+                if (child.Value is YamlMappingNode node)
+                {
+                    foreach (var nodeChild in node.Children)
+                    {
+                        var key = $"{child.Key}.{nodeChild.Key}";
+                        dataDict.Add(key, nodeChild.Value.ToString());
+                    }
+                }
+                else
+                {
+                    dataDict.Add(child.Key.ToString(), child.Value.ToString());
+                }
+            }
+
+            return dataDict;
+        }
+    }
+}
