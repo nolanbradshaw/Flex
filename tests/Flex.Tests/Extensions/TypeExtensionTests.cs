d025e896cb25878497cca5244c57cc68ffad6f44
Nolan Bradshaw <nolanbradshaw22@gmail.com>
2022-03-27 16:05:29 -0400

    quick cleanup

--- tests/Flex.Tests/Extensions/TypeExtensionTests.cs
+++ tests/Flex.Tests/Extensions/TypeExtensionTests.cs
@@ -1,10 +1,5 @@
 ﻿using Flex.Extensions;
 using Flex.Tests.Settings;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Flex.Tests.Extensions
