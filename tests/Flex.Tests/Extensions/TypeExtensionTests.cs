e28fd7abb2f7d58087367821600354a7851d9914
Nolan Bradshaw <nolanbradshaw22@gmail.com>
2022-03-27 16:01:44 -0400

    type extension tests

---
+++ tests/Flex.Tests/Extensions/TypeExtensionTests.cs
@@ -0,0 +1,74 @@
+﻿using Flex.Extensions;
+using Flex.Tests.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Flex.Tests.Extensions
+{
+    public class TypeExtensionTests
+    {
+        [Fact]
+        public void Test_SetNestedPropertyValue_SetsCorrectValue()
+        {
+            const string key = "Redis.Server";
+            var value = (object)"localhost";
+            var target = new AppSettings();
+
+            target.SetNestedPropertyValue(key, value);
+
+            Assert.Equal(value, target.Redis.Server);
+        }
+
+        [Fact]
+        public void Test_SetPropertyValue_SetsCorrectValue()
+        {
+            const string key = "AllowedHosts";
+            var value = (object)"*";
+            var target = new AppSettings();
+
+            target.SetPropertyValue(key, value);
+
+            Assert.Equal(value, target.AllowedHosts);
+        }
+
+        [Fact]
+        public void Test_SafeGetProperty_PropertyExists_ReturnsProperty()
+        {
+            const string propName = "AllowedHosts";
+            var target = new AppSettings();
+            var type = target.GetType();
+
+            var propInfo = type.SafeGetProperty(propName);
+
+            Assert.NotNull(propInfo);
+            Assert.Equal(propName, propInfo.Name);
+        }
+
+        [Fact]
+        public void Test_SafeGetProperty_PropertyDoesNotExists_ReturnsNull()
+        {
+            var target = new AppSettings();
+            var type = target.GetType();
+
+            var propInfo = type.SafeGetProperty("NonExistantProp");
+
+            Assert.Null(propInfo);
+        }
+
+        [Fact]
+        public void Test_GetPropertiesLookup_SetsCorrectNumberOfProperties()
+        {
+            var target = new AppSettings();
+            var type = target.GetType();
+
+            var lookup = type.GetPropertiesLookup();
+            var propComparison = type.GetProperties();
+
+            Assert.Equal(propComparison.Length, lookup.Count);
+        }
+    }
+}
